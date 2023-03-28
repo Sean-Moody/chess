ecfb1a9fb949a318091366271b30578ba6737892
Mohammad Naqvi <mohammadnaqvi25@gmail.com>
2023-03-28 16:03:43 -0400

    got up to phase 2

---
+++ chess_game/board.rb
@@ -0,0 +1,27 @@
+require_relative "pieces.rb"
+
+class Board
+    attr_reader :rows
+    def initialize
+        @rows = Array.new(8) {Array.new(8) {[]}}
+
+    end
+
+    def [](pos)
+        row, col = pos
+        @rows[row][col]
+    end
+
+    def []=(pos,val)
+        row,col = pos
+        @rows[row][col] << val
+    end
+
+    def move_piece(start_pos, end_pos) #setting this to check if empty, fix this to check for nullpiece later
+      if !self[start_pos].empty? 
+        self[end_pos] << self[start_pos].pop
+      end
+    end
+
+end
+
---
+++ chess_game/pieces.rb
@@ -0,0 +1,21 @@
+class Piece
+    def initialize(pos, color, board) 
+        @pos = pos
+        @color = color
+        @board = Board
+
+    end
+end
+
+class SlidingPiece < Piece
+end
+
+class SteppingPiece < Piece
+end
+
+class NullPiece < Piece
+end
+
+class Pawn < Piece
+
+end
