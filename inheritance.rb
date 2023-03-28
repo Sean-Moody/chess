e89b87fcda9c093e15288e3ecfb237ede74b7018
Mohammad Naqvi <mohammadnaqvi25@gmail.com>
2023-03-28 13:15:13 -0400

    up to bonus boss round

--- inheritance.rb
+++ inheritance.rb
@@ -11,28 +11,33 @@ class Employee
         self.salary * multiplier
     end
 
-    def boss=(boss)
-
+    def boss=(new_boss)
+        new_boss.employees << self if new_boss != nil
+        boss.employees - [self] if boss != nil
+        boss = new_boss
     end
 
-    attr_reader :salary, :boss, :name
+    attr_reader :salary, :boss, :name, :employees
 end
 
 class Manager < Employee
 
-    def initialize
-        @employees = []
+    def initialize(name, title, salary, boss = nil)
         super
+        @employees = []
     end
 
     def bonus(multiplier)
         
-
-
     end
 
     def employee=(subordinate)
-        
+        if subordinate.is_a?(Employee)
+            if !self.employees.include?(subordinate)
+                employees << subordinate
+            end
+        end
     end
 
 end
+
