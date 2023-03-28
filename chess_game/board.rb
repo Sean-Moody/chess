74d580c6cd26debb95bbbbb9a652236e46087b13
Mohammad Naqvi <mohammadnaqvi25@gmail.com>
2023-03-28 16:19:12 -0400

    fixed out board initialize

--- chess_game/board.rb
+++ chess_game/board.rb
@@ -3,8 +3,11 @@ require_relative "pieces.rb"
 class Board
     attr_reader :rows
     def initialize
-        @rows = Array.new(8) {Array.new(8) {[]}}
-
+        @rows = Array.new(8) {Array.new(8) {[nil]}}
+        @rows[0] = Array.new(8) {Piece.new()} 
+        @rows[1] = Array.new(8) {Piece.new}
+        @rows[6] = Array.new(8) {Piece.new}
+        @rows[7] = Array.new(8) {Piece.new}
     end
 
     def [](pos)
@@ -18,7 +21,7 @@ class Board
     end
 
     def move_piece(start_pos, end_pos) #setting this to check if empty, fix this to check for nullpiece later
-      if !self[start_pos].empty? 
+      if !self[start_pos].nil? 
         self[end_pos] << self[start_pos].pop
       end
     end
--- chess_game/pieces.rb
+++ chess_game/pieces.rb
@@ -1,21 +1,21 @@
 class Piece
-    def initialize(pos, color, board) 
+    def initialize(pos)#, color, board) 
         @pos = pos
-        @color = color
-        @board = Board
+        # @color = color
+        # @board = Board
 
     end
 end
 
-class SlidingPiece < Piece
-end
+# class SlidingPiece < Piece
+# end
 
-class SteppingPiece < Piece
-end
+# class SteppingPiece < Piece
+# end
 
-class NullPiece < Piece
-end
+# class NullPiece < Piece
+# end
 
-class Pawn < Piece
+# class Pawn < Piece
 
-end
+# end
